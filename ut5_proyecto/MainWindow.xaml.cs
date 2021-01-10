a3ceb42bd3f72e8700947799434227a41a15c291
FranciscoGil97 <44769277+FranciscoGil97@users.noreply.github.com>
2021-01-10 20:23:58 +0100

    Pestaña "Gestionar" aparentemente terminada.

--- ut5_proyecto/MainWindow.xaml.cs
+++ ut5_proyecto/MainWindow.xaml.cs
@@ -54,12 +54,15 @@ namespace ut5_proyecto
         private void DificultadRadioButton_Checked(object sender, RoutedEventArgs e)
         {
             RadioButton rb = sender as RadioButton;
+            if (listaPeliculas.SelectedItem != null)
+            {
+                if (rb.Tag.ToString() == dificultades[0])
+                    ((Pelicula)listaPeliculas.SelectedItem).Dificultad = dificultades[0];
+                else
+                    ((Pelicula)listaPeliculas.SelectedItem).Dificultad =
+                            rb.Tag.ToString() == dificultades[1] ? dificultades[1] : dificultades[2];
 
-            if (rb.Tag.ToString() == dificultades[0])
-                ((Pelicula)listaPeliculas.SelectedItem).Dificultad = dificultades[0];
-            else
-                ((Pelicula)listaPeliculas.SelectedItem).Dificultad =
-                        rb.Tag.ToString() == dificultades[1] ? dificultades[1] : dificultades[2];
+            }
 
         }
 
@@ -75,9 +78,9 @@ namespace ut5_proyecto
 
         private void EliminarPeliculaButton_Click(object sender, RoutedEventArgs e)
         {
-            facilRadioButton.IsChecked =  false;
+            facilRadioButton.IsChecked = false;
             normalRadioButton.IsChecked = false;
-            dificilRadioButton.IsChecked =false;
+            dificilRadioButton.IsChecked = false;
             generosComboBox.DataContext = "";
             camposGrid.DataContext = "";
             peliculas.Remove((Pelicula)listaPeliculas.SelectedItem);
@@ -109,29 +112,78 @@ namespace ut5_proyecto
         //}
 
         ////abrir un archivos mediante un dialogo
-        //private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                imagenPeliculaTextBox.Text = openFileDialog.FileName;
+        }
+
+        //private void OpenFile_Click(object sender, RoutedEventArgs e)
         //{
         //    OpenFileDialog openFileDialog = new OpenFileDialog();
         //    if (openFileDialog.ShowDialog() == true)
-        //        txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        //        imagenPeliculaTextBox.Text = File.ReadAllText(openFileDialog.FileName);
         //}
-
 
         private void ExportarButton_Click(object sender, RoutedEventArgs e)
         {
-            string personasJson = JsonConvert.SerializeObject(peliculas);
-            File.WriteAllText("peliculas.json", personasJson);
+            try
+            {
+                string personasJson = JsonConvert.SerializeObject(peliculas);
+                File.WriteAllText("peliculas.json", personasJson);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void ImportarButton_Click(object sender, RoutedEventArgs e)
         {
-            using (StreamReader jsonStream = File.OpenText("peliculas.json"))
+            try
             {
-                var json = jsonStream.ReadToEnd();
-                List<Pelicula> peliculasJson = JsonConvert.DeserializeObject<List<Pelicula>>(json);
+                using (StreamReader jsonStream = File.OpenText("peliculas.json"))
+                {
+                    var json = jsonStream.ReadToEnd();
+                    List<Pelicula> peliculasJson = JsonConvert.DeserializeObject<List<Pelicula>>(json);
 
-                foreach (Pelicula p in peliculasJson)
-                    peliculas.Add(p);
+                    foreach (Pelicula p in peliculasJson)
+                        peliculas.Add(p);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void QuitarSeleccionButton_Click(object sender, RoutedEventArgs e)
+        {
+            facilRadioButton.IsChecked = false;
+            normalRadioButton.IsChecked = false;
+            dificilRadioButton.IsChecked = false;
+            generosComboBox.DataContext = "";
+            camposGrid.DataContext = "";
+            listaPeliculas.SelectedItem = null;
+        }
+
+        private void AddPeliculaButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (listaPeliculas.SelectedItem == null)
+            {
+                string titulo = tituloPeliculaTextBox.Text;
+                string pista = pistaPeliculaTextBox.Text;
+                string imagen = imagenPeliculaTextBox.Text;
+                string genero = generosComboBox.SelectedValue != null ? generosComboBox.SelectedValue.ToString() : generos[0];
+                string dificultad;
+
+                if ((bool)facilRadioButton.IsChecked)
+                    dificultad = dificultades[0];
+                else
+                    dificultad = (bool)normalRadioButton.IsChecked ? dificultades[1] : dificultades[2];
+
+                peliculas.Add(new Pelicula(titulo, pista, imagen, dificultad, genero));
             }
         }
     }
